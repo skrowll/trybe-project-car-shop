7a9f0f1a7553d0c8cbd1143c229014426abcc8cc
skrowll <lucasdejesus.rocha@hotmail.com>
2022-11-02 05:50:07 -0300

    feat(services): create car.service

---
+++ src/services/car.service.ts
@@ -0,0 +1,31 @@
+import { IService } from '../interfaces/IService';
+import ICar, { CarZodSchema } from '../interfaces/ICar';
+import { IModel } from '../interfaces/IModel';
+
+class CarService implements IService<ICar> {
+  private _car: IModel<ICar>;
+
+  constructor(model: IModel<ICar>) {
+    this._car = model;
+  }
+
+  public async create(obj: ICar): Promise<ICar> {
+    const parsed = CarZodSchema.safeParse(obj);
+    if (!parsed.success) {
+      throw parsed.error;
+    }
+    return this._car.create(obj);
+  }
+
+  // public async read(): Promise<ICar[]> {
+  //   return this._car.read();
+  // }
+
+  // public async readOne(_id: string): Promise<ICar> {
+  //   const car = await this._car.readOne(_id);
+  //   if (!car) throw new Error('erro');
+  //   return car;
+  // }
+}
+
+export default CarService;
