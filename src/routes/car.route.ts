4f8c8b3012c9aae3f31b7bdd4a56d03396879000
skrowll <lucasdejesus.rocha@hotmail.com>
2022-11-02 05:50:53 -0300

    feat(routes): create car.route

---
+++ src/routes/car.route.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import CarController from '../controllers/car.controller';
+import CarModel from '../models/car.model';
+import CarService from '../services/car.service';
+
+const carRoute = Router();
+
+const carModel = new CarModel();
+const carService = new CarService(carModel);
+const carController = new CarController(carService);
+
+carRoute.post('/', (req, res) => carController.create(req, res));
+
+export default carRoute;
