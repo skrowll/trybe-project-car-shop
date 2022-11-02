f175ab694069f81eb2ef84f36764386d91868894
skrowll <lucasdejesus.rocha@hotmail.com>
2022-11-02 04:56:31 -0300

    test(mocks): add new mock

--- src/tests/mocks/car.mock.ts
+++ src/tests/mocks/car.mock.ts
@@ -19,4 +19,14 @@ const carMockWithId: ICar & { _id: string } = {
   doorsQty: 2,
 };
 
-export { carMock, carMockWithId };
+const updatedCarMockWithId: ICar & { _id: string } = {
+  _id: '4edd40c86762e0fb12000003',
+  model: 'Fusca',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2,
+};
+
+export { carMock, carMockWithId, updatedCarMockWithId };
