3d3eeeb6efa75a24ff6e16c922c54b3c5659c7d8
skrowll <lucasdejesus.rocha@hotmail.com>
2022-11-06 03:18:47 -0300

    feat(middlewares): create error middleware

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,24 @@
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
+import { errorCatalog, ErrorTypes } from '../errors/catalog';
+
+const errorHandler: ErrorRequestHandler = (
+  error: Error | ZodError,
+  _req,
+  res,
+  _next,
+) => {
+  if (error instanceof ZodError) {
+    return res.status(400).json({ message: error.issues });
+  }
+  const messageAsErrorType = error.message as keyof typeof ErrorTypes;
+  const mappedError = errorCatalog[messageAsErrorType];
+  if (mappedError) {
+    const { httpStatus, message } = mappedError;
+    return res.status(httpStatus).json({ message });
+  }
+  console.log(error);
+  return res.status(500).json({ message: 'internal error' });
+};
+
+export default errorHandler;
