9dc37d99cf7891405a0d14784bd125dbc3a3a89d
skrowll <lucasdejesus.rocha@hotmail.com>
2022-11-06 03:19:31 -0300

    feat(app): import and use errorHandler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import carRouter from './routes/car.route';
+import errorHandler from './middlewares/error.middleware';
+import 'express-async-errors';
 
 const app = express();
 app.use(express.json());
 
 app.use('/cars', carRouter);
+
+app.use(errorHandler);
 
 export default app;
