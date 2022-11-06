5647d4c969144074df18e9a0cc73aac86bc55daf
skrowll <lucasdejesus.rocha@hotmail.com>
2022-11-06 03:17:20 -0300

    fix(controllers): import mode od ICar

--- src/controllers/car.controller.ts
+++ src/controllers/car.controller.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from 'express';
+import { ICar } from '../interfaces/ICar';
 import { IService } from '../interfaces/IService';
-import ICar from '../interfaces/ICar';
 
 export default class CarController {
   constructor(
     private _service: IService<ICar>,
   ) { }
-
+  
   public async create(req: Request, res: Response<ICar>) {
-    const results = await this._service.create(req.body);
-    return res.status(201).json(results);
+    const result = await this._service.create(req.body);
+    return res.status(201).json(result);
   }
 }
