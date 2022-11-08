cfaa60233e13ff8ab1c8268898dc0926d911af29
skrowll <lucasdejesus.rocha@hotmail.com>
2022-11-08 00:39:43 -0300

    fix: '/motorcycle' to '/motorcycles'

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/cars', carRouter);
-app.use('/motorcycle', motorcycleRoute);
+app.use('/motorcycles', motorcycleRoute);
 
 app.use(errorHandler);
 
